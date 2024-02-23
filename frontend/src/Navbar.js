4e4bc9c02347941ada815368d11d9708aa9b0232
Jan Nordskog <jan.nordskog@gmail.com>
2024-02-24 00:30:51 +0100

    Navbar element

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
-function Navbar(){
-    return(
-        <div className="Navbar">
+function Navbar() {
+    return (
+        <div class="navbar">
+            <div className="logo">
+                <img src="http://localhost:8080/api/get/image/Logo.png" alt="Logo" width="190" height="auto"></img>
+            </div>
             <ul>
-                <li> <div className="Logo">
-            <img src="http://localhost:8080/api/get/image/Logo.png" alt="Logo"width="50" height="auto"></img>
-            </div></li>
-                <li> <Link className="NavbarLink" to="/">Home</Link></li>
-                <li> <Link className="NavbarLink" to="/hotel">Hotel</Link></li>
-
+                <li><Link className="NavbarLink" to="/">Hotell & destinasjoner</Link></li>
+                <li><Link className="NavbarLink" to="/hotel">Utvalgte tilbud</Link></li>
+                <li><Link className="NavbarLink" to="/hotel">Hotel</Link></li>
             </ul>
+            <button>Button</button>
         </div>
     );
 }
