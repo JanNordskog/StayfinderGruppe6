f0437da4c2904076c67201c0f576dbdc9038f2a3
Jan Nordskog <jan.nordskog@gmail.com>
2024-02-23 13:33:25 +0100

    Initial commit finalize

---
+++ Backend/src/main/java/no/ntnu/IDATA2306/Group6/GreetingController.java
@@ -0,0 +1,41 @@
+package no.ntnu.IDATA2306.Group6;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * A REST API controller which responds to HTTP requests for /hello.
+ */
+@RestController
+public class GreetingController {
+
+    /**
+     * Responds to HTTP GET requests for /hello.
+     *
+     * @return a greeting message
+     */
+    @GetMapping("/hello")
+    public String greetingHello() {
+        return "Hello, World!";
+    }
+
+
+    @GetMapping("/hei")
+    public ResponseEntity<String> greetingHei() {
+        // Redirect to a new location with status code 301
+        return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
+                .header("Location", "/hello")
+                .body("This endpoint has moved permanently to /hello");
+    }
+
+    @DeleteMapping("/hello")
+    public ResponseEntity<String> deleteHello() {
+        // Respond with status code 401 - Unauthorized
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+    }
+
+}
--- Backend/src/main/java/no/ntnu/IDATA2306/Group6/MainApplication.java
+++ Backend/src/main/java/no/ntnu/IDATA2306/Group6/MainApplication.java
@@ -3,10 +3,15 @@ package no.ntnu.IDATA2306.Group6;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is the main application class. It calls SprintApplication.run() where all
+ * the magic starts to happen.
+ */
 @SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
+
 }
