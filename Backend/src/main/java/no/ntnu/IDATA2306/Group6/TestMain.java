5712e49d324bf7befaf120c27fdd89bcad88ad27
Jan Nordskog <jan.nordskog@gmail.com>
2024-04-08 13:01:08 +0200

    Added test for agencies

--- Backend/src/main/java/no/ntnu/IDATA2306/Group6/TestMain.java
+++ Backend/src/main/java/no/ntnu/IDATA2306/Group6/TestMain.java
@@ -2,14 +2,16 @@ package no.ntnu.IDATA2306.Group6;
 
 import java.sql.SQLException;
 import java.util.List;
-
+/*
+ * This class is used for testing the database connection and retrieve the different table
+ */
 public class TestMain {
     public static void main(String[] args) {
         try {
-     
+            // Fetch users
             List<User> userList = new DatabaseConnection().getUsers();
 
-          
+            // Print user details
             System.out.println("User Details:");
             for (User user : userList) {
                 System.out.println("Name: " + user.getName());
@@ -22,10 +24,10 @@ public class TestMain {
                 System.out.println("\n");
             }
 
-           
+            // Fetch hotels
             List<Hotel> hotelList = new DatabaseConnection().getHotels();
 
-          
+            // Print hotel details
             System.out.println("Hotel Details:");
             for (Hotel hotel : hotelList) {
                 System.out.println("Hotel ID: " + hotel.getHotelID());
@@ -36,6 +38,20 @@ public class TestMain {
                 System.out.println("Location Type: " + hotel.getLocationType());
                 System.out.println("Room Type Available: " + hotel.getRoomTypeAvailable());
                 System.out.println("Extra Features: " + hotel.getExtraFeatures());
+                System.out.println("\n");
+            }
+
+            // Fetch agencies
+            List<Agency> agencyList = new DatabaseConnection().getAgencies();
+
+            // Print agency details
+            System.out.println("Agency Details:");
+            for (Agency agency : agencyList) {
+                System.out.println("Agency ID: " + agency.getAgencyID());
+                System.out.println("Name: " + agency.getName());
+                System.out.println("Email: " + agency.getEmail());
+                System.out.println("Phone Number: " + agency.getPhoneNumber());
+                System.out.println("Website: " + agency.getWebsite());
                 System.out.println("\n");
             }
         } catch (SQLException e) {
