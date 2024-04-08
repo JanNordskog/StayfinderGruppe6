16e768269bb00888f76fd2f15b21912d8d5ab045
Jan Nordskog <jan.nordskog@gmail.com>
2024-04-08 12:15:25 +0200

    Hotel entity class added

---
+++ Backend/src/main/java/no/ntnu/IDATA2306/Group6/Hotel.java
@@ -0,0 +1,61 @@
+package no.ntnu.IDATA2306.Group6;
+
+public class Hotel {
+
+    private String hotelID;
+    private String name;
+    private String address ;
+    private String website;
+    private String phoneNumber;
+    private String locationType;
+    private String roomTypeAvailable;
+    private String extraFeatures;
+
+    public Hotel(String hotelId, String name, String address, String website,
+    String phoneNumber, String locationType, String roomTypeAvailable, String extraFeatures ){
+        this.hotelID = hotelId;
+        this.name = name;
+        this.address = address;
+        this.website = website;
+        this.phoneNumber = phoneNumber;
+        this.locationType = locationType;
+        this.roomTypeAvailable = roomTypeAvailable;
+        this.extraFeatures = extraFeatures;
+    }
+
+    
+    public String getHotelID() {
+        return hotelID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getLocationType() {
+        return locationType;
+    }
+
+    public String getRoomTypeAvailable() {
+        return roomTypeAvailable;
+    }
+
+    public String getExtraFeatures() {
+        return extraFeatures;
+    }
+
+
+    
+}
---
+++ Backend/src/main/java/no/ntnu/IDATA2306/Group6/HotelController.java
@@ -0,0 +1,5 @@
+package no.ntnu.IDATA2306.Group6;
+
+public class HotelController {
+    
+}
