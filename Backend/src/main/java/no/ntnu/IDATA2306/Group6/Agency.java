f5e1845e58c6f22ad9485b21dd8aa139c4706b91
Jan Nordskog <jan.nordskog@gmail.com>
2024-04-08 12:50:51 +0200

    Agency entity class

---
+++ Backend/src/main/java/no/ntnu/IDATA2306/Group6/Agency.java
@@ -0,0 +1,39 @@
+package no.ntnu.IDATA2306.Group6;
+
+public class Agency {
+    private String agencyID;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String website;
+
+  
+    public Agency(String agencyID, String name, String email, String phoneNumber, String website) {
+        this.agencyID = agencyID;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.website = website;
+    }
+
+    
+    public String getAgencyID() {
+        return agencyID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+}
