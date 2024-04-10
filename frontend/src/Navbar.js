1865de75211a592b343be065c6f2cf1cc1c282e9
UlrikRibler <89103076+UlrikRibler@users.noreply.github.com>
2024-04-10 22:51:00 +0200

    Mobile optimimalized the Navbar (Navbar.js & Navbar.css)

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,22 +1,35 @@
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleMenu = () => {
+        setIsActive(!isActive);
+    };
+
     return (
-        <div class="navbar">
+        <div className={`navbar ${isActive ? 'active' : ''}`}>
             <div className="logo">
-            <Link className="NavbarLink" to="/">  <img src="http://localhost:8080/api/get/image/LogoIcon.png" alt="Logo" width="60" height="auto" /></Link>
-
-           
-
+                <Link className="NavbarLink" to="/">
+                    <img src="http://localhost:8080/api/get/image/LogoIcon.png" alt="Logo" />
+                </Link>
             </div>
-            <ul>
+            <div className="hamburger" onClick={toggleMenu}>
+                <img src="http://localhost:8080/api/get/image/Hamburgermenu.png" alt="Menu" />
+            </div>
+            <ul className={isActive ? 'nav-links-mobile' : 'nav-links'} onClick={() => setIsActive(false)}>
                 <li><Link className="NavbarLink" to="/hotellDestinasjoner">Hotell & destinasjoner</Link></li>
                 <li><Link className="NavbarLink" to="/UtvalgteTilbud">Utvalgte tilbud</Link></li>
                 <li><Link className="NavbarLink" to="/hotel">Hotel</Link></li>
+                <Link className="NavbarLink3" to="/Login"><button>Logg inn</button></Link>
             </ul>
-            <Link className="NavbarLink" to="/Login"><button>Logg inn</button></Link>
+            <div className="logg-inn-desktop">
+                <Link className="NavbarLink2" to="/Login"><button>Logg inn</button></Link>
+            </div>
         </div>
     );
 }
+
 export default Navbar;
