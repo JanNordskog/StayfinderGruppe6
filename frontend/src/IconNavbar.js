fb8ea720e76fc3f8261e1be8ee6325ac3b441c87
UlrikRibler <89103076+UlrikRibler@users.noreply.github.com>
2024-04-12 17:05:27 +0200

    Added features in Navbar.js and Navbar.css

--- frontend/src/IconNavbar.js
+++ frontend/src/IconNavbar.js
@@ -1,18 +1,27 @@
-import React from "react";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
-import axios from "axios"; // Make sure axios is imported if it's needed for API calls
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./IconNavbar.css";
 
 const IconNavbar = () => {
   const navigate = useNavigate(); // Hook to programmatically navigate
-  const today = new Date().toISOString().split("T")[0];
-  // Similar function to `handleNavigateToOslo` for the "Pool" link
+  const navRef = useRef(null);
+
+  // Function to handle horizontal scrolling
+  const scroll = (direction) => {
+    if (direction === 'left') {
+      navRef.current.scrollBy({ left: -200, behavior: 'smooth' });
+    } else {
+      navRef.current.scrollBy({ left: 200, behavior: 'smooth' });
+    }
+  };
+
+  // Async functions to handle navigation and data fetching
   const handleNavigateToPool = async () => {
     try {
-      // Corrected to match the backend controller's base path and query parameter
       const response = await axios.get("http://localhost:8080/extra", {
         params: {
-          extraFeatures: "pool", // Using 'bar' as an example extra feature
+          extraFeatures: "pool",
         },
       });
       navigate("/searchResults", { state: { data: response.data } });
@@ -23,10 +32,9 @@ const IconNavbar = () => {
 
   const handleNavigateToBar = async () => {
     try {
-      // Corrected to match the backend controller's base path and query parameter
       const response = await axios.get("http://localhost:8080/extra", {
         params: {
-          extraFeatures: "Bar", // Using 'bar' as an example extra feature
+          extraFeatures: "bar",
         },
       });
       navigate("/searchResults", { state: { data: response.data } });
@@ -35,52 +43,220 @@ const IconNavbar = () => {
     }
   };
 
+  const handleNavigateToSpa = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/extra", {
+        params: {
+          extraFeatures: "spa",
+        },
+      });
+      navigate("/searchResults", { state: { data: response.data } });
+    } catch (error) {
+      console.error("Failed to fetch listings based on extra features:", error);
+    }
+  };
+
+  const handleNavigateToResort = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/extra", {
+        params: {
+          extraFeatures: "resort",
+        },
+      });
+      navigate("/searchResults", { state: { data: response.data } });
+    } catch (error) {
+      console.error("Failed to fetch listings based on extra features:", error);
+    }
+  };
+
+
+  const handleNavigateToBusinessCenter = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/extra", {
+        params: {
+          extraFeatures: "business center",
+        },
+      });
+      navigate("/searchResults", { state: { data: response.data } });
+    } catch (error) {
+      console.error("Failed to fetch listings based on extra features:", error);
+    }
+  };
+
+  const handleNavigateToFitnessCenter = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/extra", {
+        params: {
+          extraFeatures: "fitness center",
+        },
+      });
+      navigate("/searchResults", { state: { data: response.data } });
+    } catch (error) {
+      console.error("Failed to fetch listings based on extra features:", error);
+    }
+  };
+
+  const handleNavigateToArtDecor = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/extra", {
+        params: {
+          extraFeatures: "art decor",
+        },
+      });
+      navigate("/searchResults", { state: { data: response.data } });
+    } catch (error) {
+      console.error("Failed to fetch listings based on extra features:", error);
+    }
+  };
+
+  const handleNavigateToGym = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/extra", {
+        params: {
+          extraFeatures: "gym",
+        },
+      });
+      navigate("/searchResults", { state: { data: response.data } });
+    } catch (error) {
+      console.error("Failed to fetch listings based on extra features:", error);
+    }
+  };
+
+  const handleNavigateToRoofTop = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/extra", {
+        params: {
+          extraFeatures: "rooftop",
+        },
+      });
+      navigate("/searchResults", { state: { data: response.data } });
+    } catch (error) {
+      console.error("Failed to fetch listings based on extra features:", error);
+    }
+  };
+
+  const handleNavigateToEcoFriendly = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/extra", {
+        params: {
+          extraFeatures: "eco-friendly",
+        },
+      });
+      navigate("/searchResults", { state: { data: response.data } });
+    } catch (error) {
+      console.error("Failed to fetch listings based on extra features:", error);
+    }
+  };
+
+  const handleNavigateToBabySitting = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/extra", {
+        params: {
+          extraFeatures: "babysitting",
+        },
+      });
+      navigate("/searchResults", { state: { data: response.data } });
+    } catch (error) {
+      console.error("Failed to fetch listings based on extra features:", error);
+    }
+  };
+
+  const handleNavigateToDining = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/extra", {
+        params: {
+          extraFeatures: "dining",
+        },
+      });
+      navigate("/searchResults", { state: { data: response.data } });
+    } catch (error) {
+      console.error("Failed to fetch listings based on extra features:", error);
+    }
+  };
+
+  // JSX for component rendering
   return (
-    <>
-      <nav className="IconNavbar">
-        <ul>
-          <li>
-            {/* Convert to <a> element with onClick handler */}
-            <a className="NavbarLink" onClick={handleNavigateToPool} href="#!">
-              <img
-                src="http://localhost:8080/api/get/image/HouseIcon.png"
-                alt="Home"
-              />
-              Pool
+      <>
+        <nav className="IconNavbar">
+          <img src="http://localhost:8080/api/get/image/Leftarrow.png" className="arrow left-arrow" onClick={() => scroll('left')} alt="Scroll Left" />
+          <ul ref={navRef}>
+            <li>
+              <a className="NavbarLink" onClick={handleNavigateToPool} href="#!">
+                <img src="http://localhost:8080/api/get/image/Pool.png" alt="Pool" />
+                Pool
+              </a>
+            </li>
+            <li>
+              <a className="NavbarLink" onClick={handleNavigateToBar} href="#!">
+                <img src="http://localhost:8080/api/get/image/Bar.png" alt="Bar" />
+                Bar
+              </a>
+            </li>
+            <li>
+              <a className="NavbarLink" onClick={handleNavigateToSpa} href="#!">
+                <img src="http://localhost:8080/api/get/image/Spa.png" alt="Spa" />
+                Spa
+              </a>
+            </li>
+            <li>
+              <a className="NavbarLink" onClick={handleNavigateToResort} href="#!">
+              <img src="http://localhost:8080/api/get/image/Resort.png" alt="Resort"/>
+              Resort
             </a>
-          </li>
-          {/* Other links remain unchanged */}
-          <li>
-            <a className="NavbarLink" href="/Home">
-              <img
-                src="http://localhost:8080/api/get/image/HouseIcon.png"
-                alt="Home"
-              />
-              Hotell & destinasjoner
+            </li>
+            <li>
+              <a className="NavbarLink" onClick={handleNavigateToBusinessCenter} href="#!">
+              <img src="http://localhost:8080/api/get/image/BusinessCenter.png" alt="Business Center"/>
+                Business Center
             </a>
-          </li>
-          <li>
-            <a className="NavbarLink" href="/Home">
-              <img
-                src="http://localhost:8080/api/get/image/HouseIcon.png"
-                alt="Home"
-              />
-              Hotell & destinasjoner
+            </li>
+            <li>
+              <a className="NavbarLink" onClick={handleNavigateToFitnessCenter} href="#!">
+              <img src="http://localhost:8080/api/get/image/FitnessCenter.png" alt="Fitness Center"/>
+                Fitness Center
             </a>
-          </li>
-          <li>
-            <a className="NavbarLink" href="/Home">
-              <img
-                src="http://localhost:8080/api/get/image/HouseIcon.png"
-                alt="Home"
-              />
-              Hotell & destinasjoner
+            </li>
+            <li>
+              <a className="NavbarLink" onClick={handleNavigateToArtDecor} href="#!">
+              <img src="http://localhost:8080/api/get/image/ArtDecor.png" alt="Art Decor"/>
+                Art Decor
             </a>
-          </li>
-        </ul>
-      </nav>
-      <hr />
-    </>
+            </li>
+            <li>
+              <a className="NavbarLink" onClick={handleNavigateToGym} href="#!">
+              <img src="http://localhost:8080/api/get/image/Gym.png" alt="Gym"/>
+                Gym
+            </a>
+            </li>
+            <li>
+              <a className="NavbarLink" onClick={handleNavigateToRoofTop} href="#!">
+                <img src="http://localhost:8080/api/get/image/RoofTop.png" alt="Roof Top"/>
+                Roof Top
+              </a>
+            </li>
+            <li>
+              <a className="NavbarLink" onClick={handleNavigateToEcoFriendly} href="#!">
+                <img src="http://localhost:8080/api/get/image/EcoFriendly.png" alt="Eco-Friendly"/>
+                Eco-friendly
+              </a>
+            </li>
+            <li>
+              <a className="NavbarLink" onClick={handleNavigateToBabySitting} href="#!">
+                <img src="http://localhost:8080/api/get/image/BabySitting.png" alt="Babysitting"/>
+                Babysitting
+              </a>
+            </li>
+            <li>
+              <a className="NavbarLink" onClick={handleNavigateToDining} href="#!">
+                <img src="http://localhost:8080/api/get/image/Dining.png" alt="Dining"/>
+                Dining
+              </a>
+            </li>
+          </ul>
+          <img src="http://localhost:8080/api/get/image/Rightarrow.png" className="arrow right-arrow" onClick={() => scroll('right')} alt="Scroll Right" />
+        </nav>
+        <hr/>
+      </>
   );
 };
 
