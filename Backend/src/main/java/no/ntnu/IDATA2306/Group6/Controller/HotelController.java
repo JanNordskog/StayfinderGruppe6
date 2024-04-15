ec83ad684095eab1dba2b5a9b9810ab1cadb8906
Jan Nordskog <jan.nordskog@gmail.com>
2024-04-15 22:02:08 +0200

    hotelID endpoint added

--- Backend/src/main/java/no/ntnu/IDATA2306/Group6/Controller/HotelController.java
+++ Backend/src/main/java/no/ntnu/IDATA2306/Group6/Controller/HotelController.java
@@ -1,62 +1,33 @@
 package no.ntnu.IDATA2306.Group6.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import no.ntnu.IDATA2306.Group6.Entity.Hotel;
+import no.ntnu.IDATA2306.Group6.Repo.HotelRepo;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/hotels")
 public class HotelController {
 
-    private final Map<String, Hotel> hotels = new HashMap<>();
+    @Autowired
+    private HotelRepo hotelRepo;
 
     @GetMapping
     public Collection<Hotel> getAllHotels() {
-        return hotels.values();
-    }
-
-    @PostMapping
-    public ResponseEntity<Hotel> createHotel(@RequestBody Hotel newHotel) {
-
-        if (hotels.containsKey(newHotel.getHotelID())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        }
-        hotels.put(newHotel.getHotelID(), newHotel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newHotel);
+        return hotelRepo.findAll();
     }
 
     @GetMapping("/{hotelId}")
-    public ResponseEntity<Hotel> getHotel(@PathVariable String hotelId) {
-        Hotel hotel = hotels.get(hotelId);
-        if (hotel != null) {
-            return ResponseEntity.ok(hotel);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @PutMapping("/{hotelId}")
-    public ResponseEntity<Hotel> updateHotel(@PathVariable String hotelId, @RequestBody Hotel updatedHotel) {
-        if (!hotels.containsKey(hotelId)) {
-            return ResponseEntity.notFound().build();
-        }
-        hotels.put(hotelId, updatedHotel);
-        return ResponseEntity.ok(updatedHotel);
-    }
-
-    @DeleteMapping("/{hotelId}")
-    public ResponseEntity<Void> deleteHotel(@PathVariable String hotelId) {
-        if (!hotels.containsKey(hotelId)) {
-            return ResponseEntity.notFound().build();
-        }
-        hotels.remove(hotelId);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<Hotel> getHotelById(@PathVariable String hotelId) {
+        Optional<Hotel> hotel = hotelRepo.findById(hotelId);
+        return hotel.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
