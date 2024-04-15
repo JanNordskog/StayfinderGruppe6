95b971ad95a7e3857765971431d669019fb651e5
Jan Nordskog <jan.nordskog@gmail.com>
2024-04-15 17:29:03 +0200

    Listing repo added new variable called visible

--- Backend/src/main/java/no/ntnu/IDATA2306/Group6/Entity/Listing.java
+++ Backend/src/main/java/no/ntnu/IDATA2306/Group6/Entity/Listing.java
@@ -39,19 +39,30 @@ public class Listing {
     @JoinColumn(name = "imageID", referencedColumnName = "imageID")
     private HotelImages hotelImages;
 
+    @Column(name = "visible", nullable = false, columnDefinition = "integer default 1")
+    private int visible;
+
     public Listing() {
     }
 
     public Listing(String listingID, Hotel hotel, @NotNull Agency agency, Date arrivalDate, Date departureDate,
-            double price,
-            HotelImages hotelImages) {
+            double price, HotelImages hotelImages) {
         this.listingID = listingID;
-        this.agency = agency;
         this.hotel = hotel;
-        this.hotelImages = hotelImages;
+        this.agency = agency;
         this.arrivalDate = arrivalDate;
         this.departureDate = departureDate;
         this.price = price;
+        this.hotelImages = hotelImages;
+        this.visible = 1; // Setting default value in constructor
+    }
+
+    public int getVisible() {
+        return visible;
+    }
+
+    public void setVisible(int visible) {
+        this.visible = visible;
     }
 
     public String getListingID() {
@@ -102,7 +113,7 @@ public class Listing {
     public String toString() {
         return "Listing [listingID=" + listingID + ", hotelID=" + hotel + ", agency=" + agency + ", arrivalDate="
                 + arrivalDate + ", departureDate="
-                + departureDate + ", price=" + price + ", imageLink=" + hotelImages + "]";
+                + departureDate + ", price=" + price + ", imageLink=" + hotelImages + ", visible=" + visible + "]";
     }
 
     public Agency getAgency() {
--- Backend/src/main/java/no/ntnu/IDATA2306/Group6/Repo/ListingRepo.java
+++ Backend/src/main/java/no/ntnu/IDATA2306/Group6/Repo/ListingRepo.java
@@ -9,15 +9,13 @@ import java.util.Date;
 import java.util.List;
 
 public interface ListingRepo extends JpaRepository<Listing, String> {
-        @Query("SELECT l FROM Listing l JOIN l.hotel h WHERE h.address LIKE %:address% OR h.name LIKE %:address%")
+        @Query("SELECT l FROM Listing l JOIN l.hotel h WHERE (h.address LIKE %:address% OR h.name LIKE %:address%) AND l.visible = 1")
         List<Listing> findByHotelAddress(String address);
 
-        @Query("SELECT l FROM Listing l JOIN l.hotel h WHERE (h.address LIKE %:destination% OR h.name LIKE %:destination%) AND l.arrivalDate <= :departureDate AND l.departureDate >= :arrivalDate")
+        @Query("SELECT l FROM Listing l JOIN l.hotel h WHERE (h.address LIKE %:destination% OR h.name LIKE %:destination%) AND l.arrivalDate <= :departureDate AND l.departureDate >= :arrivalDate AND l.visible = 1")
         List<Listing> findByDestinationAndDate(@Param("destination") String destination,
                         @Param("arrivalDate") Date arrivalDate, @Param("departureDate") Date departureDate);
 
-        // In your ListingRepo interface
-        @Query("SELECT l FROM Listing l WHERE l.hotel.extraFeatures LIKE %:extraFeatures%")
+        @Query("SELECT l FROM Listing l WHERE l.hotel.extraFeatures LIKE %:extraFeatures% AND l.visible = 1")
         List<Listing> findByHotelExtraFeatures(@Param("extraFeatures") String extraFeatures);
-
 }
