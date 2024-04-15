4e208a1bac8b16d065f32ccc29d7cdac963a0df9
avnit-1302 <122270703+avnit-1302@users.noreply.github.com>
2024-04-15 23:55:30 +0200

    brotha jan aboutus

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -1,11 +1,26 @@
+import React from 'react';
 import Footer from "../Footer";
+import './AboutUs.css'; // Assuming you have an accompanying CSS file for styling
 
 function AboutUs() {
-  return (
-    <div className="hotel">
-      <h1>dette er about us</h1>
-    </div>
-  );
+    return (
+        <div className="hotel">
+            <h1>About us:</h1>
+            <div className="flip-card">
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                        <img src="http://localhost:8080/api/get/image/jan.jpg" alt="Avatar" style={{ width: '300px', height: '300px' }} />
+                    </div>
+                    <div className="flip-card-back">
+                        <h1>Jan Nordskog</h1>
+                        <p>Architect & Engineer</p>
+                        <p>We love that guy</p>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default AboutUs;
