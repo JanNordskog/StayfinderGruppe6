95827b7fda75e67e0db5278ef4a1f5878b1831e0
avnit-1302 <122270703+avnit-1302@users.noreply.github.com>
2024-04-16 15:32:35 +0200

    fixed css and flip card height

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -42,7 +42,18 @@ function AboutUs() {
                     </div>
                 </div>
             </div>
-
+            <div className="flip-card">
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                        <img src="http://localhost:8080/api/get/image/avnit.png" alt="Avatar" style={{ width: '300px', height: '300px' }} />
+                    </div>
+                    <div className="flip-card-back">
+                        <h1>Avnit Simon Singh Anand</h1>
+                        <p>Architect & Engineer</p>
+                        <p>We love that guy</p>
+                    </div>
+                </div>
+            </div>
             <Footer />
         </div>
     );
