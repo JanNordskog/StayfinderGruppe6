3ed1988aa093d2e768b99e263afe615ea80ed104
UlrikRibler <89103076+UlrikRibler@users.noreply.github.com>
2024-05-11 14:14:24 +0200

    Updating HotelImagesController.java

--- Backend/src/main/java/no/ntnu/IDATA2306/Group6/Controller/HotelImagesController.java
+++ Backend/src/main/java/no/ntnu/IDATA2306/Group6/Controller/HotelImagesController.java
@@ -1,3 +1,7 @@
+/**
+ * The HotelImagesController class handles HTTP requests related to hotel images.
+ * It provides endpoints for managing hotel images.
+ */
 package no.ntnu.IDATA2306.Group6.Controller;
 
 import org.springframework.web.bind.annotation.*;
