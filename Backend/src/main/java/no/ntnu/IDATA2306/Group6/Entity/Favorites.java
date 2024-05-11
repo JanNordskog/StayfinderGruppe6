3fee46e61211ce4f0da6855004667108c2763e4e
UlrikRibler <89103076+UlrikRibler@users.noreply.github.com>
2024-05-11 15:03:15 +0200

    Updating Favorites.java

--- Backend/src/main/java/no/ntnu/IDATA2306/Group6/Entity/Favorites.java
+++ Backend/src/main/java/no/ntnu/IDATA2306/Group6/Entity/Favorites.java
@@ -1,3 +1,6 @@
+/**
+ * The Favorites class represents a favorite entity.
+ */
 package no.ntnu.IDATA2306.Group6.Entity;
 
 import jakarta.persistence.*;
@@ -22,43 +25,87 @@ public class Favorites {
     @JoinColumn(name = "listingid", nullable = false, referencedColumnName = "listingid")
     private Listing listing;
 
-    // Constructors
+    /**
+     * Constructs a new Favorites object.
+     */
     public Favorites() {
     }
 
+    /**
+     * Constructs a new Favorites object with user and listing.
+     *
+     * @param user    The user associated with the favorite
+     * @param listing The listing associated with the favorite
+     */
     public Favorites(User user, Listing listing) {
         this.user = user;
         this.listing = listing;
     }
 
-    // Getter and Setter for favoriteID
+    /**
+     * Gets the ID of the favorite.
+     *
+     * @return The favorite ID
+     */
     public Integer getFavoriteID() {
+
         return favoriteID;
     }
 
+    /**
+     * Sets the ID of the favorite.
+     *
+     * @param favoriteID The favorite ID to set
+     */
     public void setFavoriteID(Integer favoriteID) {
         this.favoriteID = favoriteID;
     }
 
-    // Getter and Setter for user
+    /**
+     * Gets the user associated with the favorite.
+     *
+     * @return The user associated with the favorite
+     */
     public User getUser() {
+
         return user;
     }
 
+    /**
+     * Sets the user associated with the favorite.
+     *
+     * @param user The user to set
+     */
     public void setUser(User user) {
+
         this.user = user;
     }
 
-    // Getter and Setter for listing
+    /**
+     * Gets the listing associated with the favorite.
+     *
+     * @return The listing associated with the favorite
+     */
     public Listing getListing() {
+
         return listing;
     }
 
+    /**
+     * Sets the listing associated with the favorite.
+     *
+     * @param listing The listing to set
+     */
     public void setListing(Listing listing) {
+
         this.listing = listing;
     }
 
-    // To String method
+    /**
+     * Returns a string representation of the Favorites object.
+     *
+     * @return A string representation of the Favorites object
+     */
     @Override
     public String toString() {
         return "Favorites{" +
