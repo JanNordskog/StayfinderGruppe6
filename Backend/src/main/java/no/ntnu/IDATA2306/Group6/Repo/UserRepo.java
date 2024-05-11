777a57dfe0eb46d4c627390d69babc84a559f03d
UlrikRibler <89103076+UlrikRibler@users.noreply.github.com>
2024-05-11 16:54:45 +0200

    Updating UserRepo.java

--- Backend/src/main/java/no/ntnu/IDATA2306/Group6/Repo/UserRepo.java
+++ Backend/src/main/java/no/ntnu/IDATA2306/Group6/Repo/UserRepo.java
@@ -1,3 +1,6 @@
+/**
+ * The UserRepo interface provides access to user data in the database.
+ */
 package no.ntnu.IDATA2306.Group6.Repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -5,11 +8,31 @@ import org.springframework.data.jpa.repository.Query;
 import no.ntnu.IDATA2306.Group6.Entity.User;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
+
+  /**
+   * Retrieves a user by their email and password.
+   *
+   * @param name     The email or username of the user
+   * @param password The password of the user
+   * @return The user matching the provided email/username and password
+   */
     @Query("SELECT u FROM User u WHERE (u.name = :name or u.email = :name) AND u.password = :password")
     User findByEmailAndPassword(String name, String password);
 
-  User findByEmail(String name);
+    /**
+   * Retrieves a user by their email.
+   *
+   * @param name The email of the user
+   * @return The user matching the provided email
+   */
+    User findByEmail(String name);
 
-
+  /**
+   * Retrieves a user by their email or username.
+   *
+   * @param name The email or username of the user
+   * @param email The email of the user
+   * @return The user matching the provided email or username
+   */
   User findOneByEmailOrName(String name, String email);
 }
