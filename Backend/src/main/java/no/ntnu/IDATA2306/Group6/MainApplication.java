590e4e9fa673fdbfd7282abfc3df0a9ccb9f1ee0
UlrikRibler <89103076+UlrikRibler@users.noreply.github.com>
2024-05-12 15:13:34 +0200

    Updating MainApplication.java

--- Backend/src/main/java/no/ntnu/IDATA2306/Group6/MainApplication.java
+++ Backend/src/main/java/no/ntnu/IDATA2306/Group6/MainApplication.java
@@ -1,3 +1,6 @@
+/**
+ * The MainApplication class serves as the entry point for the Spring Boot application.
+ */
 package no.ntnu.IDATA2306.Group6;
 
 import org.springframework.boot.SpringApplication;
@@ -8,6 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication {
 
+    /**
+     * The main method to start the Spring Boot application.
+     *
+     * @param args The command line arguments.
+     */
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
