d23d27be6018440c3df15b2353b571f4e2c3bbad
avnit-1302 <122270703+avnit-1302@users.noreply.github.com>
2024-05-20 16:30:46 +0200

    change

--- Backend/src/main/java/no/ntnu/IDATA2306/Group6/Controller/OrderController.java
+++ Backend/src/main/java/no/ntnu/IDATA2306/Group6/Controller/OrderController.java
@@ -1,45 +1,45 @@
+
 package no.ntnu.IDATA2306.Group6.Controller;
 
-import no.ntnu.IDATA2306.Group6.Entity.Order;
-import no.ntnu.IDATA2306.Group6.Repo.OrderRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.bind.annotation.*;
+    import no.ntnu.IDATA2306.Group6.Entity.Order;
+    import no.ntnu.IDATA2306.Group6.Repo.OrderRepo;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.mail.SimpleMailMessage;
+    import org.springframework.mail.javamail.JavaMailSender;
+    import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
 
   @Autowired
-  private OrderRepo orderRepo;
+  private OrderRepo orderRepository;
 
   @Autowired
-  private JavaMailSender emailSender;
+  private JavaMailSender mailSender;
 
   @PostMapping
-  public ResponseEntity<Order> createOrder(@RequestBody Order order) {
-    System.out.println("Received order: " + order); // Debug log
-    Order savedOrder = orderRepo.save(order);
-    System.out.println("Saved order: " + savedOrder); // Debug log
-    sendOrderSummaryEmail(order);
-    return ResponseEntity.ok(savedOrder);
+  public Order createOrder(@RequestBody Order order) {
+    Order savedOrder = orderRepository.save(order);
+    sendOrderSummaryEmail(savedOrder);
+    return savedOrder;
   }
 
   private void sendOrderSummaryEmail(Order order) {
     System.out.println("Sending email to: " + order.getEmail()); // Debug log
     SimpleMailMessage message = new SimpleMailMessage();
     message.setTo(order.getEmail());
-    message.setSubject("Booking Confirmation");
-    message.setText("Thank you for your booking!\n\n" +
+    message.setSubject("Order Confirmation");
+    message.setText("Thank you for your booking. Here are the details: \n" +
+        "Name: " + order.getName() + "\n" +
         "Hotel ID: " + order.getHotelId() + "\n" +
         "Destination: " + order.getDestination() + "\n" +
         "Guests: " + order.getGuests() + "\n" +
-        "Dates: " + order.getArrivalDate() + " - " + order.getDepartureDate() + "\n" +
-        "Price: $" + order.getPrice() + "\n\n" +
-        "Please don't hesitate to contact us if you have any questions.");
-    emailSender.send(message);
+        "Arrival Date: " + order.getArrivalDate() + "\n" +
+        "Departure Date: " + order.getDepartureDate() + "\n" +
+        "Price: $" + order.getPrice());
+    mailSender.send(message);
     System.out.println("Email sent"); // Debug log
   }
 }
+
