2fc5f832932f7ce23f4cf995896a09af20b33316
Jan Nordskog <jan.nordskog@gmail.com>
2024-05-20 17:43:52 +0200

    added disclaimer to the footer of the webiste

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -3,29 +3,44 @@ import { Link } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <div className="Footer">
-            <div className="section left-section">
-
-                <p>Sign up for newsletter to get discounted offers!</p>
-                <form>
-                    <input type="text" placeholder="Enter your details" />
-                    <button type="submit">Submit</button>
-                    <p>By signing up, you agree to our terms and conditions.</p>
-                </form>
-            </div>
-            <div className="section right-section">
-                <img src="http://localhost:8080/api/get/image/Logo.png" alt="Right Image" />
-                <h2>Support</h2>
-                <ul>
-                    <li><Link to="/help">Help Centre</Link></li>
-                    <li><Link to="/faqs">FAQs</Link></li>
-                    <li><Link to="/track-order">Track your Order</Link></li>
-                    <li><Link to="/contact">Contact Us</Link></li>
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className="Footer">
+      <div className="section left-section">
+        <p>Sign up for newsletter to get discounted offers!</p>
+        <form>
+          <input type="text" placeholder="Enter your details" />
+          <button type="submit">Submit</button>
+          <p>
+            This website is a result of a university group project, performed in
+            the course IDATA2301 Web technologies, at NTNU. All the information
+            provided here is a result of imagination. Any resemblance with real
+            companies or products is a coincidence.
+          </p>
+        </form>
+      </div>
+      <div className="section right-section">
+        <img
+          src="http://localhost:8080/api/get/image/Logo.png"
+          alt="Right Image"
+        />
+        <h2>Support</h2>
+        <ul>
+          <li>
+            <Link to="/help">Help Centre</Link>
+          </li>
+          <li>
+            <Link to="/faqs">FAQs</Link>
+          </li>
+          <li>
+            <Link to="/track-order">Track your Order</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
